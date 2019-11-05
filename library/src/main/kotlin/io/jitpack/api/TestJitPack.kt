76f7fdc5444bc02ca250dc85603de6c4a52a1e92
wangzhiguo <wangzhiguo13120140@163.com>
2019-11-05 11:18:08 +0800

    test jitpack

---
+++ library/src/main/kotlin/io/jitpack/api/TestJitPack.kt
@@ -0,0 +1,11 @@
+package io.jitpack.api
+
+import android.util.Log
+
+class TestJitPack {
+    companion object {
+        fun helloJitPack() {
+            Log.e("TestJitPack", "hello jit pack！");
+        }
+    }
+}
