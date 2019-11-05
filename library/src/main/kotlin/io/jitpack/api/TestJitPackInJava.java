66ce881a16db7e76529b8a6c0e5d88be58bda5c7
wangzhiguo <wangzhiguo13120140@163.com>
2019-11-05 12:53:21 +0800

    Add Java test

---
+++ library/src/main/kotlin/io/jitpack/api/TestJitPackInJava.java
@@ -0,0 +1,10 @@
+package io.jitpack.api;
+
+import android.util.Log;
+
+public class TestJitPackInJava {
+
+    public static void testJitPack() {
+        Log.e("TestJitPackInJava", "hello jit pack!");
+    }
+}
